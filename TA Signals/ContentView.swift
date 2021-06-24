5e3f6b0172a2d2ab9a647ef45d91759ce9c96c8c
iamgabrielma <gabrielmaldonado.art@gmail.com>
2021-06-24 11:41:51 +0700

    Initial Commit

---
+++ TA Signals/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TA Signals
+//
+//  Created by Gabriel Maldonado Almendra on 24/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TA Signals/TA_SignalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TA_SignalsApp.swift
+//  TA Signals
+//
+//  Created by Gabriel Maldonado Almendra on 24/6/21.
+//
+
+import SwiftUI
+
+@main
+struct TA_SignalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
