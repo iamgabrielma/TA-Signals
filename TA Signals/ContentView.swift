98c57a34645354045dee323ad5a85c4320ab6944
iamgabrielma <gabrielmaldonado.art@gmail.com>
2021-07-26 21:52:49 +0700

    Removed test_styling() and completed StockListCell View

--- TA Signals/ContentView.swift
+++ TA Signals/ContentView.swift
@@ -22,23 +22,6 @@ struct ContentView: View {
             return "closed"
         }
     }
-    // TODO: https://github.com/iamgabrielma/TA-Signals/issues/6
-    func test_styling(item: String, type: String) -> Text {
-        
-        var styledSignal : Text = Text(item)
-        
-        if type == "signal" {
-            styledSignal = Text(item).foregroundColor(.red)
-        }
-        else if type == "rsi" {
-            styledSignal = Text("RSI: \(item)" as String)
-        }
-        else {
-            styledSignal = Text(item)
-        }
-        
-        return styledSignal
-    }
     
     func fetchUpdate() -> Void{
         // TODO: https://github.com/iamgabrielma/TA-Signals/issues/7
@@ -54,27 +37,13 @@ struct ContentView: View {
                     Text("Market is \(openOrClosed(b: isMarketOpen))").font(.caption)
                     Text("Now: \(now, style: .date)").font(.caption)
                     Text("Last Fetch: June 25, 2021").font(.caption)
+                    
                     if showFetchDetails {
                         // 2. A list is created containing the todo items
                         List(fetchedObject.stocks) { stock in
-                            // Implement custom view via StockListCell.swift:
                             StockListCell(stock: stock)
-//                            VStack(alignment: .leading){
-//                                HStack(spacing: 25){
-//                                    Text(stock.ticker)
-//                                    (test_styling(item: stock.signal, type: "signal"))
-//                                    VStack(){
-//                                        (test_styling(item: stock.rsi, type: "rsi"))
-//                                        Text("EMA100: \(stock.ema100)")
-//                                        Text("EMA200: \(stock.ema200)")
-//                                    }
-//                                }
-//                                Divider()
-//                            }
-                        }// <!-- List
+                        }
                         Text("This is not financial or investment advice. The Content is for informational purposes only.").font(.caption)
-                    //} //! -- Section && Text attempt
-
                     }
                 }
                 .navigationTitle("TA Signals")
--- TA Signals/StockListCell.swift
+++ TA Signals/StockListCell.swift
@@ -16,9 +16,9 @@ struct StockListCell: View {
         VStack(alignment: .leading){
             HStack(spacing: 25){
                 Text(stock.ticker)
-                //(test_styling(item: stock.signal, type: "signal"))
+                Text(stock.signal)
                 VStack(){
-                    //(test_styling(item: stock.rsi, type: "rsi"))
+                    Text("RSI: \(stock.rsi)")
                     Text("EMA100: \(stock.ema100)")
                     Text("EMA200: \(stock.ema200)")
                 }
@@ -30,8 +30,7 @@ struct StockListCell: View {
 
 struct StockListCell_Previews: PreviewProvider {
     static var previews: some View {
-        // 1 - Error: From what I understand here, I should pass the instance of Stock() but is throwing: Missing argument for parameter 'from' in call. Is it expecting the JSOn decoder?
-        // 2- Seems I need to pass 1 stock like: StockListCell(stock: Stock(id: T##Int, ticker: T##String, rsi: T##String, ema100: T##String, ema200: T##String, signal: T##String)), is it because it needs and initializer, and I have not set default values in the struct declaration?
-        StockListCell(stock: Stock(id: 0, ticker: "none", rsi: "0", ema100: "0", ema200: "0", signal: "None"))
+        
+        StockListCell(stock: Stock(id: 0, ticker: "NONE", rsi: "0", ema100: "0", ema200: "0", signal: "Neutral"))
     }
 }
